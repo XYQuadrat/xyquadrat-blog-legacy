6f6eda030a8157d7d3636a3514898109c0007238
xyquadrat <mail@xyquadrat.ch>
2021-08-27 19:06:16 +0200

    Add simple fractions generator

---
+++ fractions.js
@@ -0,0 +1,45 @@
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
+
+// center the drawing
+var x = canvas.width / 2;
+var y = canvas.height / 2;
+
+// number of days
+var days = 1;
+
+// number of hours
+var numerator = document.getElementById('numerator');
+var denominator = document.getElementById('denominator');
+
+// how thick you want a segment
+var segmentDepth = 240;
+
+function drawFraction() {
+    context.clearRect(0,0, canvas.width, canvas.height)
+    for (var i = 1; i <= days; i++) {
+        drawSegments(i * segmentDepth); 
+    }
+}
+
+function drawSegments(radius) {
+		var pieAngle = 2 * Math.PI / denominator.value;
+
+    for (var i = 0; i < denominator.value; i++) {
+        context.beginPath();
+        context.moveTo(x, y);
+        context.arc(x, y, radius, i*pieAngle, (i+1)*pieAngle, false);
+        context.lineWidth = segmentDepth;
+        var hueValue = i * 15;
+        if(i < numerator.value){
+           context.fillStyle = 'hsl(' + hueValue + ',70%, 60%)';
+           context.fill();
+        }
+
+        context.lineWidth = 2;
+        context.strokeStyle = '#444';
+        context.stroke();
+    }
+}
+
+drawFraction()

8f22188b1aac46e7207526faee7c8d341856e639
xyquadrat <mail@xyquadrat.ch>
2021-08-28 14:14:10 +0200

    Add line fraction

--- fractions.js
+++ fractions.js
@@ -1,5 +1,7 @@
 var circleCanvas = document.getElementById('circleCanvas');
 var rectCanvas = document.getElementById('rectCanvas');
+var lineCanvas = document.getElementById('lineCanvas');
+var line = lineCanvas.getContext('2d');
 var circle = circleCanvas.getContext('2d');
 var rect = rectCanvas.getContext('2d');
 
@@ -14,17 +16,20 @@ var segmentDepth = 240;
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('numerator').addEventListener('change', drawFraction);
-    document.getElementById('denominator').addEventListener('change', drawFraction)
+    document.getElementById('denominator').addEventListener('change', drawFraction);
     document.getElementById('downloadCircleImg').addEventListener('click', downloadCircle, false);
-    document.getElementById('downloadRectImg').addEventListener('click', downloadRect, false)
+    document.getElementById('downloadRectImg').addEventListener('click', downloadRect, false);
+    document.getElementById('downloadLineImg').addEventListener('click', downloadLine, false);
 })
 
 function drawFraction() {
     circle.clearRect(0, 0, circleCanvas.width, circleCanvas.height)
     rect.clearRect(0, 0, rectCanvas.width, rectCanvas.height);
+    line.clearRect(0, 0, lineCanvas.width, lineCanvas.height);
 
     drawCircle(segmentDepth);
     drawRectangle();
+    drawLine();
 }
 
 function drawCircle(radius) {
@@ -39,6 +44,7 @@ function drawCircle(radius) {
         circle.arc(x, y, radius, i * pieAngle, (i + 1) * pieAngle, false);
         circle.lineWidth = segmentDepth;
         setStyle(i, circle);
+        circle.stroke();
     }
 }
 
@@ -50,6 +56,33 @@ function drawRectangle() {
         rect.rect(i * width + 1, rectCanvas.height / 4, width, rectCanvas.height / 2);
 
         setStyle(i, rect);
+        rect.stroke();
+    }
+}
+
+function drawLine() {
+    var lineLength = lineCanvas.width - 10;
+    var marginLeft = 5;
+
+    line.strokeStyle = '#444';
+    line.lineWidth = 2;
+    line.beginPath();
+    line.moveTo(marginLeft, lineCanvas.height/2);
+    line.lineTo(marginLeft + lineLength, lineCanvas.height/2);
+    line.stroke();
+
+    line.beginPath();
+    line.moveTo(marginLeft, lineCanvas.height/2);
+    line.lineTo(marginLeft + Math.min(lineLength / denominator.value * numerator.value, lineLength), lineCanvas.height/2);
+    line.strokeStyle = 'hsl(40,70%, 60%)';
+    line.stroke();
+
+    for(var i = 0; i <= denominator.value; ++i) {
+        line.beginPath();
+        line.moveTo(marginLeft + i * lineLength / denominator.value, lineCanvas.height /2 - 6);
+        line.lineTo(marginLeft + i * lineLength / denominator.value, lineCanvas.height /2 + 6);
+        line.strokeStyle = '#444';
+        line.stroke();
     }
 }
 
@@ -62,7 +95,6 @@ function setStyle(i, context) {
 
     context.lineWidth = 2;
     context.strokeStyle = '#444';
-    context.stroke();
 }
 
 function downloadCircle(){
@@ -72,6 +104,11 @@ function downloadCircle(){
 
 function downloadRect() {
     var data = rectCanvas.toDataURL('image/png');
+    this.href = data;
+}
+
+function downloadLine() {
+    var data = lineCanvas.toDataURL('image/png');
     this.href = data;
 }
 
